f2e9c946899af948a5be8c3825688bd2d7414595
Jc Abalos <jc4balos@gmail.com>
2026-03-26 13:38:00 +0800

    created role model

---
+++ src/main/java/com/jc4balos/user_service/model/Role.java
@@ -0,0 +1,51 @@
+package com.jc4balos.user_service.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "role", indexes = {
+        @Index(name = "username_index", columnList = "username"),
+        @Index(name = "email_index", columnList = "email"),
+        @Index(name = "uuid_index", columnList = "user_uuid")
+})
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "role_id")
+    private Long roleId;
+
+    @Column(nullable = false, name = "role_uuid", unique = true, length = 36)
+    private String roleUUID;
+
+    @Column(nullable = false, name = "role_name")
+    private String roleName;
+
+    @Column(nullable = false, name = "role_desc")
+    private String roleDescription;
+
+    @Column(nullable = false, name = "is_active")
+    private Boolean isActive;
+
+    @PrePersist
+    public void generateUUID() {
+        this.roleUUID = UUID.randomUUID().toString();
+    }
+}
