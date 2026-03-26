12e5e47fd555a7289081d84b44c87ceedf2748ff
Jc Abalos <jc4balos@gmail.com>
2026-03-26 13:22:15 +0800

    added newrole dto

---
+++ src/main/java/com/jc4balos/user_service/dto/request/role/NewRoleDto.java
@@ -0,0 +1,20 @@
+package com.jc4balos.user_service.dto.request.role;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NewRoleDto {
+
+    @NotBlank(message = "Role is not valid.")
+    private String roleName;
+
+    @NotBlank(message = "Role description is not valid.")
+    private String roleDescription;
+}
